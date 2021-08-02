5ec3041a030c8b02992601cb2b7281d4ebfdb644
Rodrigo Souza <rodrigo-souza-dev@outlook.com>
2021-08-01 21:47:54 -0300

    project complete

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('.bt-hamb').addEventListener('click', function () {
+    document.querySelector(".menu-mo").style.display = "block";
+  })
+  document.querySelector(".menu-close").addEventListener('click', function () {
+    document.querySelector('.menu-mo').style.display = "none";
+  })
